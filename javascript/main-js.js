e5983939813c3a66d401279a1619dccc48557f8e
mindo2a <m.koritauskas@yahoo.com>
2017-04-14 15:53:24 +0300

    Ikeliu savo projekta

---
+++ javascript/main-js.js
@@ -0,0 +1,124 @@
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 54.901, lng: 23.880},
+          zoom: 12,
+          styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+        });
+
+var icons = {
+    commercial: {
+        icon:  'images/office-building.png'
+    },
+    residental: {
+        icon: 'images/apartment.png'
+    },
+    industry: {
+        icon: 'images/factory.png'
+    },
+    publicPlaces: {
+        icon: 'images/embassy.png'
+    }
+};    
+
+var features = [
+   {
+    position: new google.maps.LatLng(54.901980, 23.880304),
+    type: 'residental'
+   }, {
+    position: new google.maps.LatLng(54.903830, 23.895626),
+    type: 'commercial'
+   }
+];
+
+features.forEach(function(feature) {
+    var marker = new google.maps.Marker({
+        position: feature.position,
+        icon: icons[feature.type].icon,
+        map: map
+    });
+});
+        
+}
+
+
+
+
---
+++ javascript/project.js
@@ -0,0 +1,20 @@
+function addGalleryItems () {
+	var newItems = '<div class="gallery-item">\
+                    <a href="">\
+                    <figure class="image">\
+                    <img src="http://placeimg.com/640/480/arch" alt="architecture" style="display: block;">\
+                    </figure>\
+                    <div class="desc">Add comment here</div>\
+                    </a>\
+                    </div>';
+        $('.gallery').append(newItems);
+    }   
+     
+    var isEnd = true,
+        i = 0;
+
+    while (i < 100) {
+    	addGalleryItems();
+    	i++;
+    }             
+
