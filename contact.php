8856fbcb38bf7570af4fa53c99a018bb8990dd35
mindo2a <m.koritauskas@yahoo.com>
2017-04-21 23:02:04 +0300

    sutvarkyta forma/galima kelt duomenis

--- contact.php
+++ contact.php
@@ -46,8 +46,10 @@
                                 <option name="Statomas" value="Statomas">Statomas</option>
                                 <option name="Planuojamas" value="Planuojamas">Planuojamas</option>
                                 <option name="Pastatytas" value="Pastatytas">Pastatytas</option>
+                                <option name="Rekonstruojamas" value="Rekonstruojamas">Rekonstruojamas</option>
                             </select>
                             <hr>
+                            <!-- Apie Projekta -->
                             <h3>Apie projekta: </h3>
                             <p class="p-style">Projekta vysto</p>
                             <input class="about-prj" type="text" name="develop"
@@ -55,20 +57,25 @@
                             <p class="p-style">Architektas</p>
                             <input class="about-prj" type="text" name="arch">
                             <p class="p-style">Bendras plotas</p>
-                            <input class="plotas" type="text" name="plotas">
+                            <input class="plotas" type="text" name="bl-size">
                             <p class="p-style">Tipas:</p>
-                            <select class="tipas" name="type">
+                            <select class="tipas" name="type-selector">
                                 <option name= "Komercinis" value="komercinis">Komercinis</option>
                                 <option name="Gyvenamieji" value="gyvenamieji">Gyvenamieji</option>
                                 <option name="Gamyba/Logistika" value="gamyba">Gamyba/Logistika</option>
                                 <option name="Visuomeninis" value="visuomeninis">Visuomeninis</option>
+                                <option name="medicina" value="medicina">Sveikatos priežiūra</option>
+                                <option name="svietimas" value="svietimas">Švietimo įstaiga</option>
+                                <option name="parkas" value="parkai">Parkai</option>
+                                <option name="viesos" value="viesos">Viešosios erdvės</option>
+                                <option name="transportas" value="transportas">Transportas</option>
                             </select>
                             <p class="p-style">Projekto tinklapis</p>
                             <input class="tinklapis" type="text" name="web-page"><br>
                             <p class="p-style">Projekto aprašas:</p>
-                            <textarea class="text-area" name="text" rows="5" cols="50"></textarea>
+                            <textarea class="text-area" name="text" rows="5" cols="75"></textarea>
                             <p class="p-style">Nuotraukos:</p>
-                            <input type="file" name="file[]" multiple="multiple">
+                            <input type="file" id="file" name="files[]" multiple="multiple" accept="image/*" />
                             <hr>
                             <h3>Jūsu kontaktai:</h3>
                             <p class="p-style">Vardas/Pavardė</p>
@@ -96,7 +103,9 @@
     </section>
     <?php
     
-    $con = mysqli_connect("localhost", "root", "root", "mano_projektas");
+    $con = mysqli_connect("localhost", "root", "root", "projectdb");
+
+    mysqli_set_charset($con,"utf8");
 
     if( $con === false ) {
         die ("ERROR: Could not connect. ".mysqli_connect_error());
@@ -104,22 +113,83 @@
     
     if(isset($_POST['submit'])) {
     
-
+    //formos dalis iki apie projekta  
     $project = mysqli_real_escape_string($con, $_POST['project']);
     $city = mysqli_real_escape_string($con, $_POST['city']);
     $street = mysqli_real_escape_string($con, $_POST['street']);
     $latitude = mysqli_real_escape_string($con, $_POST['latitude']);
     $longitude = mysqli_real_escape_string($con, $_POST['longitude']);
     $data = mysqli_real_escape_string($con, $_POST['data']);  
-    // $selector = mysqli_real_escape_string($con, $_POST['selector']);
+    $develop = mysqli_real_escape_string($con, $_POST['develop']);
+    $arch = mysqli_real_escape_string($con, $_POST['arch']);
+    $size = mysqli_real_escape_string($con, $_POST['bl-size']);
+    $web_page = mysqli_real_escape_string($con, $_POST['web-page']);
+    $text = mysqli_real_escape_string($con, $_POST['text']);
 
 
-    if(isset($_POST['selector'])){
+    if(isset($_POST['selector']) and isset($_POST['type-selector'])){
         $selector = $_POST['selector'];
-        $sql = "INSERT INTO vieta (project,city,street,latitude,longitude,data,status) VALUES ('$project','$city','$street','$latitude','$longitude','$data','$selector');";
-         mysqli_query($con, $sql);                    
+        $type_selector = $_POST['type-selector'];
+        $sql = "INSERT INTO place (project,city,street,latitude,longitude,data_,category,develop,arch,size,type,web_page,text_area) VALUES ('$project','$city','$street','$latitude','$longitude','$data','$selector','$develop','$arch','$size','$type_selector','$web_page','$text');";
+         mysqli_query($con, $sql);
+         $lastPlaceId = mysqli_insert_id ($con);                    
     }
+    // formos dalis apie projekta
+
+
+    // if(isset($_POST['type-selector'])){
+    //     $type_selector = $_POST['type-selector'];
+    //     $sql .= "INSERT INTO place (develop,arch,size,type,web_page,text_area) VALUES ('$develop','$arch','$size','$type_selector','$web_page','$text');";
+    //      mysqli_query($con, $sql);                    
+    // }
+
+    // Images upload 
     
+    $valid_formats = array("jpg", "png", "gif",);
+    $max_file_size = 3*1024*1024; 
+    $path = "uploads/"; // Upload directory
+    $count = 0;
+
+    if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
+        // Loop $_FILES to exeicute all files
+        foreach ($_FILES['files']['name'] as $f => $name) {     
+            if ($_FILES['files']['error'][$f] == 4) {
+                continue; // Skip file if any error found
+            }          
+            if ($_FILES['files']['error'][$f] == 0) {              
+                if ($_FILES['files']['size'][$f] > $max_file_size) {
+                    $message[] = "$name is too large!.";
+                    continue; // Skip large files
+                }
+                elseif( ! in_array(pathinfo($name, PATHINFO_EXTENSION), $valid_formats) ){
+                    $message[] = "$name is not a valid format";
+                    continue; // Skip invalid file formats
+                }
+                else{ // No error found! Move uploaded files 
+                    $sql_error = ''; // add this before you start the loop
+                    $img_name = $name;
+
+                    if(move_uploaded_file($_FILES["files"]["tmp_name"][$f], $path.$name)){
+                      $sql = "INSERT INTO images (image_name,place_id) VALUES ('$img_name', $lastPlaceId)" ;
+
+                      $result = mysqli_query($con, $sql);
+                      if ( false===$result ) {
+                        $sql_error .= 'Error in the query '.$sql.'  Error Desc :'.mysqli_error($con).'<br /><br />' ;
+                      }
+                    }
+                    }
+            }
+        }
+    } 
+    
+    $name_lname = mysqli_real_escape_string($con, $_POST['name']);
+    $company = mysqli_real_escape_string($con, $_POST['company']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+
+    $sql = "INSERT INTO contacts (name,company,email,phone,place_id) VALUES ('$name_lname','$company','$email','$phone','$lastPlaceId')";
+
+
     if (mysqli_multi_query($con, $sql)) {
         echo "New records created successfully";
     } else {
